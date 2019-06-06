e976ffa0372a6cd849c81f934b0b77401da5912f
Administrator <506394403@qq.com>
2019-06-06 13:40:07 +0800

    6.6修改bug2

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
-        <dome></dome>
+        <!--<dome></dome>-->
         <!--<dome2></dome2>-->
+        <dome3></dome3>
     </div>
 </template>
 
 <script>
     import dome from '../overview/videoDome'
+    import dome3 from '../overview/handheld'
 
     export default {
         name: "index",
         components: {
             dome,
+            dome3
         }
     }
 </script>
---
+++ src/views/overview/handheld.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{qaq}}
+    </div>
+</template>
+
+<script>
+    import {handheld} from "common/js/handheld";
+
+    export default {
+        data() {
+            return {
+                qaq: [],
+            }
+        },
+        mounted() {
+            handheld().then(res => {
+                console.log(res);
+                this.qaq = res;
+            })
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
