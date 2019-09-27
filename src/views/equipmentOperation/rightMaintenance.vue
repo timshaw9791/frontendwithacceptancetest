e321f008167dfa8608a2e2f0def8ded7115522d6
sup.me <1035924544@qq.com>
2019-09-27 15:21:02 +0800

    修复入库界面点击确认无法入库BUG

--- src/views/equipmentOperation/rightMaintenance.vue
+++ src/views/equipmentOperation/rightMaintenance.vue
@@ -98,6 +98,7 @@
                         console.log(res);
                         this.callback('入库成功!');
                         this.equipList = [];
+                        this.$refs.StorageDialog.hide()
                     })
                 } else {
                     this.$message.error('未选择装备!')
@@ -126,20 +127,7 @@
 
             },
             DialogShow() {
-                // this.$message.error("该装备不属于装备保养清单！")
-                console.log(this.list)
-                console.log(this.list.equip)
-                this.list = this.list.map(item => {
-                    // return Object.assign(item, {rfidConfirm: -1}) // -1为初始 无显示， 1为勾， 0为感叹号
-                    this.$set(item, "rfidConfirm", -1); // 对返回对象进行响应式属性添加
-                    return item;
-                });
-                console.log(this.list)
-                console.log(this.list.equip)
                 this.$refs.StorageDialog.show();
-                setTimeout(() => {
-                    this.startRfid("19080011");
-                }, 5000);
             },
         }
     }
