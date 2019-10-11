d848336d7efb245e89b71bbfe26ed2b141bf65f2
sup.me <1035924544@qq.com>
2019-10-11 16:06:45 +0800

    申请文字颜色，人员出入记录倒序

--- src/views/process/directAdjustment.vue
+++ src/views/process/directAdjustment.vue
@@ -19,7 +19,7 @@
                 </div>
                 <div style="margin-left: 38px;cursor: pointer" @click="addDirectAdjustment" v-if="viewStatus.singleFlag.apply">
                     <svg-icon icon-class='加' class="icon-search"></svg-icon>
-                    <span>申请直调</span>
+                    <span style="color: #2F2F76">申请直调</span>
                 </div>
                 <div class="_buttons" style="margin-right: 18px" v-if="havePage">
                     <BosInput
