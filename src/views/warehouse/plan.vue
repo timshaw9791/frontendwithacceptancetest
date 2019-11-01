6b8295fc652d3bd59d04d94fa067453712b76f66
Lieatfhy <1986808565@qq.com>
2019-11-01 17:00:59 +0800

    #119 更正

--- src/views/warehouse/plan.vue
+++ src/views/warehouse/plan.vue
@@ -189,7 +189,6 @@
                     console.log(item);
                     this.form = JSON.parse(JSON.stringify(item));
                     this.form.planEquips.push({equipModel: '', location: {}});
-                    this.form['planEquips'] = [{equipModel: '', location: {}}];
                 }
                 this.getEquipInfo();
 
