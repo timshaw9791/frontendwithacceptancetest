6b0e73a5267aa0d6799185877d82d62460acbe1c
sup.me <1035924544@qq.com>
2019-11-06 14:28:21 +0800

    预案管理新增 装备型号没有数据 #146

--- src/views/warehouse/plan.vue
+++ src/views/warehouse/plan.vue
@@ -211,7 +211,9 @@
             },
             getEquipInfo() {
                 getEquipList().then(res => {
-                    let newData = JSON.parse(JSON.stringify(res.data.EquipList.content));
+                    console.log('--------------------------');
+                    console.log(res);
+                    let newData = JSON.parse(JSON.stringify(res.content));
                     let eqName = newData.map(res => {
                         return res.equipArg.model
                     });
