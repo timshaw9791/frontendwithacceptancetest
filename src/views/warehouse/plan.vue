68431937c6958e0052db082312627ca14541707e
sup.me <1035924544@qq.com>
2019-11-07 11:32:27 +0800

    新增预案或不能删除不现实其他内容或同时删除的问题

--- src/views/warehouse/plan.vue
+++ src/views/warehouse/plan.vue
@@ -147,7 +147,6 @@
                 restaurants: [],
                 title: '',
                 delId: '',
-                nowRow:{},
             }
         },
         mixins: [formRulesMixin, transformMixin],
@@ -162,13 +161,16 @@
             },
 
             qaq(row, data) {
-                this.nowRow=data.key
-                // this.form.planEquips[row.$index] = data.key;
-                // this.form.planEquips[row.$index] = JSON.parse(JSON.stringify(data.key));
-
+                let tempData = JSON.parse(JSON.stringify(data))
+                //this.form.planEquips[row] = data.key;
+                this.form.planEquips[row.$index].equipModel = data.key.equipModel
+                this.form.planEquips[row.$index].equipName = data.key.equipName
+                this.form.planEquips[row.$index].location = data.key.location
+                
                 if (row.$index === this.form.planEquips.length - 1) {
                     this.form.planEquips.push({equipModel: '', location: {}});
                 }
+
             },
 
             delqaq(row) {
@@ -211,8 +213,6 @@
             },
             getEquipInfo() {
                 getEquipList().then(res => {
-                    console.log('--------------------------');
-                    console.log(res);
                     let newData = JSON.parse(JSON.stringify(res.content));
                     let eqName = newData.map(res => {
                         return res.equipArg.model
