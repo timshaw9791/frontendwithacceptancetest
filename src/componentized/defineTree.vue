88781e158a93fb26cdcca74afcd43f4a55bcc283
SprWu <1035924544@qq.com>
2020-04-14 08:05:31 +0800

    树形组件

---
+++ src/componentized/defineTree.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="define-tree-container">
+    <el-tree :data="data" :empty-text="placeHolder" :props="prop" :default-expand-all="expandAll" :accordion="accordion"></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'defineTree',
+  props: {
+    data: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    placeHolder: { // 当无数据时显示的文字
+      type: String,
+      default: '暂无内容'
+    },
+    prop: { // 节点渲染树形配置
+      type: Object,
+      default() {
+        return {
+          label: 'label',
+          children: 'children'
+        }
+      }
+    },
+    expandAll: { // 是否默认展开所有节点
+      type: Boolean,
+      default: true
+    },
+    accordion: { // 是否同时只能展开一级 手风琴模式
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
