e04680fe299ec3a3605a537a9600e0b97914909e
SprWu <1035924544@qq.com>
2020-04-28 11:10:17 +0800

    axios实例代码修整

--- vue.config.js
+++ vue.config.js
@@ -44,7 +44,6 @@ module.exports = {
                 'base': '@/base',
                 'api': '@/api',
                 'store': '@/store',
-                'gql': '@/graphql',
             }
         }
     },
