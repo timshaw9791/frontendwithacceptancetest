7c1ef3321d1ff7b0ce1d4e117c8b6ee5ba8fac91
SprWu <1035924544@qq.com>
2020-04-29 16:02:29 +0800

    文件上传组件边框判断逻辑

--- src/components/layout/components/Sidebar/personal.vue
+++ src/components/layout/components/Sidebar/personal.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <img :src="imageUrl">
+        <!-- <img :src="imageUrl"> -->
+        <upload-file v-model="form.faceInformation" size="small" :disabled="true" margin="24px 0 15px 0"></upload-file>
         <div class="info">
             <span>{{form.name}}</span>
             <span>{{form.position}}</span>
@@ -13,21 +14,25 @@
     import {imgBaseUrl} from "api/config";
     import {getOrganUnitById} from 'api/process'
     import { userInfoById } from 'api/user'
+    import uploadFile from '@/componentized/uploadFile'
     export default {
+        components:{uploadFile},
         data() {
             return {
-                form: {organUnitName:'',name:'',position:"啊啊啊"},
+                form: {organUnitName:'',name:'',position:"啊啊啊", faceInformation: ''},
                 imageUrl: '',
             }
         },
         methods: {
             fetchData() {
                 let data = JSON.parse(localStorage.getItem('user'));
-                userInfoById(data.id).then(res=>{
-                    this.form.position = res.position;
-                    this.imageUrl = `${imgBaseUrl}${res.faceInformation}`;
-                });
-                this.form = Object.assign(this.form, data);
+                this.form = data;
+                console.log(imgBaseUrl+data.faceInformation);
+                // userInfoById(data.id).then(res=>{
+                //     this.form.position = res.position;
+                //     this.imageUrl = `${imgBaseUrl}${res.faceInformation}`;
+                // });
+                // this.form = Object.assign(this.form, data);
             },
         },
         mounted() {
@@ -35,3 +40,12 @@
         }
     }
 </script>
+
+<style scoped>
+    .faceinfomation {
+        width: 0.4792rem;
+        height: 0.4792rem;
+        border-radius: 50%;
+        margin: 0.125rem 0 0.0833rem 0;
+    }
+</style>
