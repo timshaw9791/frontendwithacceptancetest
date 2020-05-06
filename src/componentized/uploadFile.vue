280c318b770d1ca997653d989359e4e7e617332f
SprWu <1035924544@qq.com>
2020-05-06 11:13:13 +0800

    文件上传进度值转化为Number

--- src/componentized/uploadFile.vue
+++ src/componentized/uploadFile.vue
@@ -34,7 +34,7 @@ export default {
                     'Content-Type': 'multipart/form-data'
                 },
                 onUploadProgress: event => {
-                    this.percentage = (event.loaded/event.total*100).toFixed(1);
+                    this.percentage = Number((event.loaded/event.total*100).toFixed(1));
                 }
             }
         }
