71d2014135ae2894a4d2d7fed99237d15c0c2a83
youlik <1789324739.@qq.com>
2020-05-06 11:45:39 +0800

    增加判断  防止rfid 重复添加

--- src/views/equipmentOperation/startService.vue
+++ src/views/equipmentOperation/startService.vue
@@ -132,9 +132,12 @@ export default {
             },
              classDataify(data)//读写器数据处理的方法
             {
+                    if(this._.findIndex(this.list,data[0])==-1)//避免重复
+                {
                 data.forEach(item=>{this.list.push(item)})
                 let cList=this._.groupBy(this.list, item => `${item.equipArg.model}${item.equipArg.name}${item.location.id}`)
                 this.newData=this._.map(cList,(v,k)=>{return {equipArg:v[0].equipArg,copyList:v,count:v.length,location:v[0].location}})
+                }
             },
             cancel(){
                 this.$router.back()
