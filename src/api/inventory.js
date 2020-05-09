736a3c701c667501c796faf9ea2eb8ad281022f4
hellocx <1004711601@qq.com>
2020-05-09 11:42:07 +0800

    盘点操作

--- src/api/inventory.js
+++ src/api/inventory.js
@@ -1,9 +1,9 @@
 /*盘点接口*/
 import request from 'common/js/request'
-
+// data 里面需要 inventoryOrder 和 rfids 列表
 function inventoryOrder(method, data) {
     let requestObj = {
-        url: '/inventory-order',
+        url: '/inventory-orders',
         method: method
     }
     switch (method) {
@@ -12,7 +12,8 @@ function inventoryOrder(method, data) {
             break
         }
         case 'post':{
-            requestObj.params = data
+            requestObj.data = data.inventoryOrder
+            requestObj.url = `/inventory-orders?rfids=${data.rfids}`
             break
         }
     }
