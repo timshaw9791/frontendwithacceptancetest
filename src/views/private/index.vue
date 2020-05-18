36d9fa7451569d87daaa1612dd62022d408a1f1a
SprWu <1035924544@qq.com>
2020-05-18 17:14:01 +0800

    个人中心验证、defineForm完善_1

--- src/views/private/index.vue
+++ src/views/private/index.vue
@@ -2,19 +2,19 @@
     <div class="personal-container">
       <my-header title="个人中心"></my-header>
       <div class="body">
-          <div class="left-info">
+          <define-form :column="6" ref="form">
               <define-input label="姓名" v-model="userInfo.name" :column="6" :disabled="true" margin="10px 5px"></define-input>
               <checkbox label="权限" :column="6" v-model="userInfo.enterHouse" margin="10px 5px" :disabled="true">开门库房</checkbox>    
               <radio label="性别" :column="6" v-model="userInfo.gender" :data="genderList" :disabled="true" margin="10px 5px"></radio>
               <define-input label="职位" :column="6" v-model="userInfo.position" :disabled="true" margin="10px 5px"></define-input><br>
               <define-input label="身份证号" :column="6" v-model="userInfo.idNumber" :disabled="true" margin="10px 5px"></define-input>
-              <define-input label="警号(账号)" :column="6" v-model="userInfo.policeSign" :disabled="true" margin="10px 5px"></define-input><br>
-              <define-input label="联系电话" :column="6" type="Phone"  v-model="userInfo.phone" :disabled="disabled" margin="10px 5px"></define-input>
-              <define-input label="密码" :column="6" v-model="userInfo.password" :disabled="disabled" margin="10px 5px"></define-input><br>
+              <define-input label="警号(账号)" :column="6" v-model="userInfo.policeSign" :disabled="true" margin="10px 5px"></define-input>
+              <define-input label="联系电话" :column="6" type="Phone"  v-model="userInfo.phone" :disabled="disabled" margin="10px 5px" valid></define-input>
+              <define-input label="密码" :column="6" v-model="userInfo.password" :disabled="disabled" margin="10px 5px"></define-input>
               <define-input label="机构单位" :column="6" v-model="userInfo.organUnitName" :disabled="true" margin="10px 5px"></define-input>
-              <define-input label="指纹信息" :column="6" v-model="userInfo.fingerprintInformation" :disabled="true" margin="10px 5px"></define-input><br>
+              <define-input label="指纹信息" :column="6" v-model="userInfo.fingerprintInformation" :disabled="true" margin="10px 5px"></define-input>
               <radio label="角色" :column="6" v-model="userInfo.role" :data="roleList" :disabled="true" margin="10px 5px"></radio>
-          </div>
+          </define-form>
           <div class="right-image">
               <upload-file v-model="userInfo.faceInformation" :disabled="disabled" size="large"></upload-file>
           </div>
@@ -55,10 +55,15 @@ export default({
     },
     methods: {
         submit() {
-            editUser(this.userInfo.id, this.userInfo).then(res => {
-                this.userInfo = JSON.parse(JSON.stringify(res));
-                localStorage.setItem('user', JSON.stringify(res));
-                this.disabled = true;
+            if(this.disabled) return
+            this.$refs.form.validate(valid => {
+                if(valid) {
+                    editUser(this.userInfo.id, this.userInfo).then(res => {
+                        this.userInfo = JSON.parse(JSON.stringify(res));
+                        localStorage.setItem('user', JSON.stringify(res));
+                        this.disabled = true;
+                    })
+                }
             })
         }
     }
@@ -75,9 +80,6 @@ export default({
         min-width: 1000px;
         justify-content:space-around;
         margin: 50px auto;
-    }
-    .left-info {
-        width: 700px;
     }
     .footer {
         text-align: center;
