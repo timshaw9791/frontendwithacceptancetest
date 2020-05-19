defab8e95273f202ba223e4c041e689c16970d33
SprWu <1035924544@qq.com>
2020-05-19 15:01:41 +0800

    消息中心一键已读

--- src/api/message.js
+++ src/api/message.js
@@ -1,7 +1,7 @@
 import request from 'common/js/request'
 var qs = require('qs')
 
-export function readMsg(data) {
+export function readMsg(data) { // 消息标记为已读
     return request({
         url: `/messages/mark-reads?${qs.stringify(data,{indices:false})}`,
         method: "post",
@@ -12,5 +12,21 @@ export function getMsgList(params) {
         url: `/messages/title?${qs.stringify(params, {indices:false})}`,
         method: "GET",
         // params
+    })
+}
+
+export function markStar(params) { // 消息标星
+    return request({
+        url: '/messages/mark-star',
+        method: 'POST',
+        params
+    })
+}
+
+export function allRead(params) { // 消息一键已读
+    return request({
+        url: '/messages/to-reads',
+        method: 'POST',
+        params
     })
 }
--- src/views/message/index.vue
+++ src/views/message/index.vue
@@ -3,13 +3,16 @@
 		<my-header title="消息中心" height="45px"></my-header>
 		<div class="top">
 			<define-input label="消息标题"></define-input>
-			<base-button label="一键已读" align="right" margin="0 30px 0 0"></base-button>
+			<base-button label="一键已读" align="right" margin="0 30px 0 0" @click="allMessageRead"></base-button>
 		</div>
 		<div class="body">
 			<bos-tabs :option="['contrast']" :contrastKey="['main', 'content']" :layoutRatio="['45%', '55%']" :header="false">
 				<define-table :data="list" :pageInfo="fetchParams.pageInfo" @changePage="changePage" height="4.0104rem" 
 					:highLightCurrent="true" @changeCurrent="changeCurrent" slot="main" ref="leftTable">
-					<define-column label="操作" field="opeare" width="60"></define-column>
+					<define-column label="操作" field="opeare" width="60" v-slot="{ data }">
+						<i class="iconfont iconxingbiaotianchong star" @click="messageStar(false)" v-if="data.row.newStar"></i>
+						<i class="iconfont iconxingbiaoxianxing" @click="messageStar(true)" v-else></i>
+					</define-column>
 					<define-column label="消息状态" width="100" v-slot="{ data }">
 						<span :class="['sign',{unread: !data.row.status}]" @click="readIt(data.row)">
 							{{ data.row.status?'已读':'未读' }}
@@ -17,7 +20,6 @@
 					</define-column>
 					<define-column label="通知时间" :filter="row => this.$filterTime(row.createTime)" width="200"></define-column>
 					<define-column label="消息标题" :filter="row=>fixTitle(row.title)"></define-column>
-					<!-- <define-column label="消息内容" field="content"></define-column> -->
 				</define-table>
 				<define-table :data="list[selectIndex].messageItems" :havePage="false" height="4.2448rem" slot="content">
 					<define-column label="消息内容" field="content"></define-column>
@@ -29,7 +31,7 @@
 
 <script>
 import myHeader from 'components/base/header/header';
-import { getMsgList, readMsg } from 'api/message'
+import { readMsg, markStar, allRead } from 'api/message'
 import { bosMixin } from 'field/mixins/listMixin'
 import { jsqlPage, bosEnums } from 'api/basic'
 import bosTabs from '@/componentized/table/bosTabs'
@@ -54,17 +56,20 @@ export default {
 				params: [
 					JSON.parse(localStorage.getItem('user')).id
 				]
-			}
+			},
+			userId: JSON.parse(localStorage.getItem('user')).id
 		}
 	},
 	methods: {
 		fetchData() {
+			jsqlPage(this.fetchParams).then(res => {
+				this.list = this._.flatten(res.content);
+				this.fetchParams.pageInfo.totalPages = res.totalPages;
+			})
+		},
+		fetchEnumerator() {
 			bosEnums({category: 'MessageTitleEnum'}).then(res => {
 				this.enumerator = res;
-				jsqlPage(this.fetchParams).then(res => {
-					this.list = this._.flatten(res.content);
-					this.fetchParams.pageInfo.totalPages = res.totalPages;
-				})
 			})
 		},
 		readIt(data) {
@@ -79,7 +84,19 @@ export default {
 		fixTitle(value) {
 			let tmp = this.enumerator.find(item => item.value == value)
 			return tmp?tmp.chinese:''
+		},
+		messageStar(state) {
+			allRead
+		},
+		allMessageRead() {
+			allRead({userId: this.userId}).then(res => {
+				this.fetchData();
+				console.log(res);
+			})
 		}
+	},
+	created() {
+		this.fetchEnumerator();
 	}
 }
 </script>
@@ -97,6 +114,12 @@ export default {
 	}
 	.unread {
 		color: red;
+	}
+	.star {
+		color: #f8dc00;
+	}
+	.iconfont {
+		font-size: 14px;
 	}
 }
 </style>
