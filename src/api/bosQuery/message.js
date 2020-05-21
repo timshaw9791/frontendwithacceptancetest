5ab63a4a90303af17d7130ee1e72e74555078071
SprWu <1035924544@qq.com>
2020-05-21 14:47:25 +0800

    消息中心(消息数量)修正

--- src/api/bosQuery/message.js
+++ src/api/bosQuery/message.js
@@ -1,5 +1,5 @@
 let userJSON = JSON.parse(localStorage.getItem('user')),
-    userId = userJSON && userJSON.id || '1';
+    userId = userJSON && userJSON.id;
 export var message = {
     "jpql": "select ms from Message ms where ms.userId = ?1 order by ms.newStar desc, ms.status desc, ms.createTime desc",
     "returnType": "ARRAY",
