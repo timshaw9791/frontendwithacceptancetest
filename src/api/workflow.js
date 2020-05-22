d2a55ab2bd26f44848eafa3e0ff46fd7662dfdf1
hellocx <1004711601@qq.com>
2020-05-22 12:19:12 +0800

    报废流程

--- src/api/workflow.js
+++ src/api/workflow.js
@@ -1,5 +1,5 @@
 import request from 'common/js/request'
-
+import {baseBURL} from "./config";
 export function scrappedUp(data) {
     return request({
         url: '/equip-scrap/admin-submit',
@@ -13,3 +13,25 @@ export function scrappedApproval(data) {
         params: data
     })
 }
+
+export function taskDetail(params) {
+    return request({
+        url: baseBURL+'/workflow/task-detail',
+        method:'GET',
+        params
+    })
+}
+
+
+export function completeTask(taskId,params) {
+    return request({
+        url: baseBURL+`/workflow/v3/tasks/${taskId}/complete`,
+        method:'PUT',
+        data:params
+    })
+}
+
+
+
+
+
