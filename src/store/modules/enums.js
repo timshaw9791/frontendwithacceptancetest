0d8f2654cb8cba0cb0d33285424c391157b11094
SprWu <1035924544@qq.com>
2020-06-28 15:51:55 +0800

    添加库房端的枚举

--- src/store/modules/enums.js
+++ src/store/modules/enums.js
@@ -1,12 +1,21 @@
 /* 枚举数据 */
-import { bosEnumsPlatform } from "api/basic";
+import { bosEnums, bosEnumsPlatform } from "api/basic";
+
+function fixEnums(enums) {
+    let arr = enums.map(enumItem=>({name:enumItem.name,values:enumItem.values.map(item=>({value:item.chinese,key:item.name}))})),
+        obj = arr.map(enumItem=>({name:enumItem.name,values:enumItem.values.reduce((result,now)=>{
+                result[now.key] = now.value
+                return result
+            }, {})}))
+    return {arr, obj}
+}
 
 export default {
     state: {
         platformEnums: JSON.parse(localStorage.getItem('platformEnums')) || [], // 平台枚举数据
         platformEnumsObj: JSON.parse(localStorage.getItem('platformEnumsObj')) || [], // 可直接通过key获取value
-        enums: [], // 库房枚举数据
-        enumsObj: []
+        enums: JSON.parse(localStorage.getItem('enums')) || [], // 库房枚举数据
+        enumsObj: JSON.parse(localStorage.getItem('enumsObj')) || []
     },
     mutations: {
         setPlatformEnums(state, enums) {
@@ -25,16 +34,20 @@ export default {
     actions: {
         getAllEnums({commit}) {
             bosEnumsPlatform().then(res => {
-                let tmp = res.map(enumItem=>({name:enumItem.name,values:enumItem.values.map(item=>({value:item.chinese,key:item.name}))})),
-                    tmpObj = tmp.map(enumItem=>({name:enumItem.name,values:enumItem.values.reduce((result,now)=>{
-                            result[now.key] = now.value
-                            return result
-                        }, {})}))
-                commit('setPlatformEnums', tmp)
-                commit('setPlatformEnumsObj', tmpObj)
-                localStorage.setItem('platformEnums', JSON.stringify(tmp))
-                localStorage.setItem('platformEnumsObj', JSON.stringify(tmpObj))
-                tmp = tmpObj = null
+                let tmp = fixEnums(res)
+                commit('setPlatformEnums', tmp.arr)
+                commit('setPlatformEnumsObj', tmp.obj)
+                localStorage.setItem('platformEnums', JSON.stringify(tmp.arr))
+                localStorage.setItem('platformEnumsObj', JSON.stringify(tmp.obj))
+                tmp = null
+            })
+            bosEnums().then(res => {
+                let tmp = fixEnums(res)
+                commit('setEnums', tmp.arr)
+                commit('setEnumsObj', tmp.obj)
+                localStorage.setItem('enums', JSON.stringify(tmp.arr))
+                localStorage.setItem('enumsObj', JSON.stringify(tmp.obj))
+                tmp = null
             })
         }
     }
