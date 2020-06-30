a55aec200cc31b72ad2a12fef752828d1d79405f
SprWu <1035924544@qq.com>
2020-06-30 10:46:32 +0800

    枚举数据结构更新 Array => Object，getters获取方法更新

--- src/store/modules/enums.js
+++ src/store/modules/enums.js
@@ -2,20 +2,19 @@
 import { bosEnums, bosEnumsPlatform } from "api/basic";
 
 function fixEnums(enums) {
-    let arr = enums.map(enumItem=>({name:enumItem.name,values:enumItem.values.map(item=>({value:item.chinese,key:item.name}))})),
-        obj = arr.map(enumItem=>({name:enumItem.name,values:enumItem.values.reduce((result,now)=>{
-                result[now.key] = now.value
-                return result
-            }, {})}))
-    return {arr, obj}
+    let choice = enums.map(enumItem=>({name:enumItem.name,values:enumItem.values.map(item=>({value:item.chinese,key:item.name}))}))
+            .reduce((res, cur) => Object.assign(res, {[cur.name]: cur.values}), {}),
+        render = {};
+    for (let key in choice) {
+        render[key] = choice[key].reduce((res, cur) => Object.assign(res, {[cur.key]: cur.value}), {})
+    }
+    return {choice, render}
 }
 // 默认的枚举数据
 const initEnums = [{
-        name: 'gender',
-        values: [{value: '男', key: '男'}, {value: '女', key: '女'}]
+        gender: [{value: '男', key: '男'}, {value: '女', key: '女'}]
     }, {
-        name: 'device',
-        values: [{value: '读写器', key: 'reader'}, {value: '手持机', key: 'handheld'}]
+        device: [{value: '读写器', key: 'reader'}, {value: '手持机', key: 'handheld'}]
     }];
 // 此处避免多次判断+访问localStorage导致性能问题
 var platformEnums = localStorage.getItem('platformEnums'),
@@ -25,10 +24,10 @@ var platformEnums = localStorage.getItem('platformEnums'),
 
 export default {
     state: {
-        platformEnums: platformEnums && JSON.parse(platformEnums) || [], // 平台枚举数据
-        platformEnumsObj: platformEnumsObj && JSON.parse(platformEnumsObj) || [], // 可直接通过key获取value
-        enums: enums && JSON.parse(enums) || [], // 库房枚举数据
-        enumsObj: enumsObj && JSON.parse(enumsObj) || []
+        platformEnums: platformEnums && JSON.parse(platformEnums) || {}, // 平台枚举数据
+        platformEnumsObj: platformEnumsObj && JSON.parse(platformEnumsObj) || {}, // 可直接通过key获取value
+        enums: enums && JSON.parse(enums) || {}, // 库房枚举数据
+        enumsObj: enumsObj && JSON.parse(enumsObj) || {}
     },
     mutations: {
         setPlatformEnums(state, enums) {
@@ -48,19 +47,19 @@ export default {
         getAllEnums({commit}) {
             bosEnumsPlatform().then(res => {
                 let tmp = fixEnums(res)
-                commit('setPlatformEnums', tmp.arr)
-                commit('setPlatformEnumsObj', tmp.obj)
-                localStorage.setItem('platformEnums', JSON.stringify(tmp.arr))
-                localStorage.setItem('platformEnumsObj', JSON.stringify(tmp.obj))
+                commit('setPlatformEnums', tmp.choice)
+                commit('setPlatformEnumsObj', tmp.render)
+                localStorage.setItem('platformEnums', JSON.stringify(tmp.choice))
+                localStorage.setItem('platformEnumsObj', JSON.stringify(tmp.render))
                 tmp = null
             })
             bosEnums().then(res => {
                 let tmp = fixEnums(res)
-                tmp.arr = tmp.arr.concat(initEnums) // 加入默认的枚举数据
-                commit('setEnums', tmp.arr)
-                commit('setEnumsObj', tmp.obj)
-                localStorage.setItem('enums', JSON.stringify(tmp.arr))
-                localStorage.setItem('enumsObj', JSON.stringify(tmp.obj))
+                tmp.choice = Object.assign(tmp.choice, ...initEnums) // 加入默认的枚举数据
+                commit('setEnums', tmp.choice)
+                commit('setEnumsObj', tmp.render)
+                localStorage.setItem('enums', JSON.stringify(tmp.choice))
+                localStorage.setItem('enumsObj', JSON.stringify(tmp.render))
                 tmp = null
             })
         }
